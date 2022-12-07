8baeeb3dd152aa9119c1806e8c5af4beb09d2010
Bill Mårtensson <bill@magictechnology.se>
2022-12-07 14:33:53 +0100

    Initial Commit

---
+++ testashare/testashareApp.swift
@@ -0,0 +1,17 @@
+//
+//  testashareApp.swift
+//  testashare
+//
+//  Created by Bill Martensson on 2022-12-07.
+//
+
+import SwiftUI
+
+@main
+struct testashareApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
